605b662a198f2918796c588e150581b3cc7fff93
DevSohel32 <sujauddoulasohel352@gmail.com>
2026-01-12 20:47:24 +0600

    Updated Changes

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,15 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
-
+use App\Http\Controllers\UserController;
 
 Auth::routes();
 
-Route::get('/', [HomeController::class, 'index'])->name('home.index');
+Route::middleware(['auth'])->group(function(){
+  Route::get('/account-dashboard', [UserController::class, 'index'])->name('home.index');
+});
+  Route::middleware(['auth'])->group(function(){
+  Route::get('/account-dashboard', [UserController::class, 'index'])->name('home.index');
+});
+  
+
